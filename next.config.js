8b4af070caddb8e039dc47432985437fca5a0407
Denis DECLERCQ <denis.declercq@adevinta.com>
2024-02-02 09:29:46 +0100

    fix: css js 404

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  outDir: "./docs"
+  distDir: '/docs',
+  assetPrefix: './'
 }
    
 module.exports = nextConfig
