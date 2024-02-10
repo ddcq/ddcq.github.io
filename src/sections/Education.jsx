61459d7885eade6c18f2c1665d192db3c5b8d370
Denis DECLERCQ <denis.declercq@adevinta.com>
2024-02-10 19:12:41 +0100

    fix education align on mobile view

--- src/sections/Education.jsx
+++ src/sections/Education.jsx
@@ -1,12 +1,17 @@
 'use client'
 import Title from '../components/Title'
 import EducationCard from '../components/EducationCard'
+import { useMediaQuery } from '@uidotdev/usehooks'
 
 export default function Education() {
+  const isLargeDevice = useMediaQuery('only screen and (min-width : 1024px)')
   return (
     <section id="education">
       <Title>Formation</Title>
-      <ul className="timeline lg:grid lg:grid-cols-4 timeline-vertical lg:timeline-horizontal" style= {{'--timeline-col-start': '52px'}}>
+      <ul
+        className="timeline lg:grid lg:grid-cols-4 timeline-vertical lg:timeline-horizontal"
+        {...(!isLargeDevice && { style: { '--timeline-col-start': '52px' } })}
+      >
         <EducationCard year="2001" name="Master 2 Génie des Logiciels Applicatifs" org="UPMC" />
         <EducationCard year="2003" name="Teradata Basic, Physical Implementation, SQL, Utilitaires" />
         <EducationCard year="2013" name="Scrum Master Certification" org="SCRUM ALLIANCE, INC." />
