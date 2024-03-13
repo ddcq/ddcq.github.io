c4f55c2675e8ed1ce50a51814de2952f458b8795
Denis DECLERCQ <denis.declercq@adevinta.com>
2024-03-13 22:04:00 +0100

    fix back icon on mobile

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -15,7 +15,11 @@ export default function Breadcrumbs({ breadcrumbs }) {
       </div>
       <div>
         <a className="btn btn-accent btn-circle btn-sm" href="/">
-          ⮌
+          <svg height="24px" viewBox="-48 -14 32 32" width="24px">
+            <g>
+              <path d="M-31-6c-5,0-9,2-12,6l-5-5v16h16l-5-5c1-4,6-8,10-8c6,0,11,5,11,11C-16-0-23-6-31-6z" />
+            </g>
+          </svg>
         </a>
       </div>
     </div>
