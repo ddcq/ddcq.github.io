74793c0855a700a380f4e4f6ed7562018ef9cb59
Denis DECLERCQ <denis.declercq@adevinta.com>
2024-03-21 22:48:27 +0100

    fix bg color on text

--- app/airfrancedeclic/page.tsx
+++ app/airfrancedeclic/page.tsx
@@ -13,7 +13,7 @@ function Text({ children, title, className }: PropsWithChildren<{ title?: string
   return (
     <Parallax
       translateY={['0%', '-100%']}
-      className={cx('absolute bg-slate-50 z-20 w-screen fluid-md flex flex-col gap-vw-4-min@sm-max@lg opacity-90 p-vw-16-min@sm-max@xl', className)}
+      className={cx('absolute bg-base-200 z-20 w-screen fluid-md flex flex-col gap-vw-4-min@sm-max@lg opacity-90 p-vw-16-min@sm-max@xl', className)}
     >
       {title && <h2 className="fluid-3xl">{title}</h2>}
       {children}
