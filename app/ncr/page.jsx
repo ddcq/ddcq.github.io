b0a97a8a4596c4950d474f4c48155531f353dbbf
Denis DECLERCQ <denis.declercq@adevinta.com>
2024-04-04 23:23:03 +0200

    ncr images

--- app/ncr/page.jsx
+++ app/ncr/page.jsx
@@ -14,7 +14,7 @@ export default function NCR() {
         <h3 className="fluid-4xl text-center">Gestion de sauvegarde sur bande</h3>
         <p className="fluid-3xl text-center">Ingénieur Expert Database TERADATA</p>
       </Text>
-      <Image url="/img/ev/a.jpg" />
+      <Image url="/img/ncr/a.jpeg" />
       <Text title="Description">
         Le logiciel NetVault est une solution de sauvegarde d'entreprise conçue pour les environnements hybrides et Cloud. Il offre une protection continue des
         données, une restauration instantanée et une défense contre les cyberattaques, notamment les rançongiciels. Avec des fonctionnalités telles que les
@@ -40,7 +40,7 @@ export default function NCR() {
         <h3 className="fluid-4xl text-center">Datamart Vues Utilisateurs</h3>
         <p className="fluid-3xl text-center">Ingénieur Expert Database TERADATA</p>
       </Text>
-      <Image url="/img/ev/a.jpg" />
+      <Image url="/img/ncr/b.jpeg" />
       <Text title="Tâches">
         <ul className="list-disc">
           <li>Rédaction des spécifications fonctionnelles et détaillées (PowerAMC)</li>
@@ -54,16 +54,20 @@ export default function NCR() {
         <h3 className="fluid-4xl text-center">CRM, informatique décisionnelle pour les campagnes marketing</h3>
         <p className="fluid-3xl text-center">Ingénieur Expert Database TERADATA</p>
       </Text>
-      <Image url="/img/ev/a.jpg" />
-      <Text title="Tâches">
-        <ul className="list-disc">
+      <Image url="/img/ncr/e.jpeg" />
+      <Text title="Description">
+        <p>
+          Le CRM (Customer Relationship Management) en marketing est un outil puissant qui permet de gérer et d’analyser les interactions avec les clients pour
+          optimiser les campagnes marketing. Le CRM est utilisé pour collecter et analyser les données relatives à l'audience, ce qui facilitera le travail des
+          équipes marketing et commerciales. Teradata est un choix robuste pour l’extraction et l’analyse des données issues du CRM.
+        </p>
+        <h2 className="fluid-3xl">Tâches</h2>
+        <ul className="list-disc ml-vw-12-max@md">
           <li>Procédures d'installation et de chargement automatique des données dans le nouveau modèle Teradata (Script Unix ksh, Teradata)</li>
           <li>Génération de rapport automatique (HTML, PPWizzard, Rex)</li>
         </ul>
-      </Text>
-      <Image url="/img/ev/a.jpg" />
-      <Text title="Technologies">
-        <ul className="list-disc">
+        <h2 className="fluid-3xl">Technologies</h2>
+        <ul className="list-disc ml-vw-12-max@md">
           <li>Script Unix ksh</li>
           <li>Teradata</li>
           <li>HTML</li>
@@ -71,7 +75,7 @@ export default function NCR() {
           <li>Rex</li>
         </ul>
       </Text>
-      <Image url="/img/ev/a.jpg" />
+      <Image url="/img/ncr/d.jpeg" />
       <Text>
         <ul className="list-inline item-details">
           <li>
