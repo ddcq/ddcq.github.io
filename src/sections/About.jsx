db5fc94ea1cbc469bcff56a603416dccaea66c99
Denis DECLERCQ <denis.declercq@adevinta.com>
2024-04-08 18:33:17 +0200

    flesh out about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,58 +1,73 @@
-import { Parallax } from 'react-scroll-parallax'
-
 export default function About() {
   return (
     <section className="sm:p-24 flex flex-col gap-8 w-full" id="about">
-      <div data-aos="fade-right" className="w-full sm:w-96 card glass shadow-xl sm:card-side">
-        <figure>
+      <div data-aos="fade-right" className="w-full card glass shadow-xl sm:card-side">
+      <figure className="min-w-vw-96-min@sm-max@2xl">
           <img src="img/ia/c.webp" className="h-full" />
         </figure>
         <div className="card-body">
-          Avec près de 20 ans d’expérience dans les technologies JEE, j’ai pu suivre l’évolution des nouvelles technologies. J’ai gardé de mes premières années
-          le soucis d’un code clair, le plus optimisé possible et dimensionné au besoin.
+          Fort de plus de 20 ans d’expérience dans le domaine des technologies de l’Internet, j’ai eu l’opportunité unique de témoigner et de m’adapter à
+          l’émergence et à la progression constante de nouvelles technologies. Cette longue expérience m’a permis de développer une expertise approfondie et une
+          compréhension particulière des systèmes complexes. Dès mes débuts, j’ai cultivé une approche rigoureuse en matière de programmation, veillant à
+          produire un code non seulement clair et lisible, optimisé pour une consommation réduite des resources et également répondant précisément aux besoins
+          fonctionnelles avec comme principal objectif, la meilleure expérience utilisateur possible. Cette philosophie, alliant clarté, efficacité et
+          adaptabilité, continue de guider mon travail et mes contributions au sein de l’industrie du numérique.
         </div>
       </div>
-      <div data-aos="fade-left" className="self-end w-full sm:w-96 card sm:card-side glass shadow-xl">
-        <figure>
+      <div data-aos="fade-left" className="self-end w-full card sm:card-side glass shadow-xl">
+      <figure className="min-w-vw-96-min@sm-max@2xl">
           <img src="img/ia/b.webp" className="h-full" />
         </figure>
         <div className="card-body">
-          J’ai toujours mené ma carrière avec la volonté d’être le plus ouvert aux autres idées. J’ai souvent eu l’occasion de travailler main dans la main avec
-          la partie "Mainframe" qui dans beaucoup d’entreprises gardent encore le cœur du métier.
+          J’ai toujours piloté ma carrière avec la ferme conviction que l’ouverture aux idées des autres est la clé du progrès et de l’innovation. Cette
+          philosophie m’a permis de tisser des liens solides avec mes collègues et de collaborer efficacement, en particulier avec l’équipe Mainframe, qui est
+          souvent le pilier technologique dans de nombreuses entreprises. En travaillant main dans la main avec ces experts, j’ai pu non seulement approfondir
+          ma compréhension des systèmes centraux, mais aussi contribuer à des projets transversaux qui ont renforcé l’intégration des technologies modernes avec
+          les systèmes existants. Cette synergie a permis de créer des solutions robustes et évolutives, assurant ainsi la pérennité et la compétitivité de
+          notre entreprise dans un marché en constante évolution.
         </div>
       </div>
-      <div data-aos="fade-right" className="card md:card-side glass shadow-xl max-w-2xl">
-        <figure>
-          <img src="img/ia/a.webp" className="sm:h-full w-full" />
+      <div data-aos="fade-right" className="card xl:card-side glass shadow-xl self-end max-h-3xl">
+        <figure className="min-w-vw-96-min@sm-max@2xl">
+          <img src="img/ia/a.webp" className="md:h-full w-full" />
         </figure>
         <div className="card-body">
-          Je me suis également appliqué à perfectionner mes requêtes SQL pour soulager le serveur d’application. J’ai souhaité aussi connaître les véritables
-          problèmes que connaît la production au quotidien.
+          Je me suis consciencieusement attelé à l’optimisation de mes requêtes SQL, en mettant en œuvre des techniques avancées telles que l’indexation
+          judicieuse et la réécriture de requêtes pour minimiser la charge sur le serveur d’application. Cette démarche proactive a non seulement allégé la
+          charge de travail du serveur, mais a également entraîné une réduction significative du temps de réponse, améliorant ainsi l’expérience utilisateur.
+          Parallèlement, j’ai pris l’initiative de plonger au cœur des opérations de production pour identifier et comprendre les défis réels auxquels elle est
+          confrontée. En collaborant étroitement avec l’équipe de production, j’ai pu mettre en lumière des problématiques critiques et proposer des solutions
+          concrètes qui ont contribué à améliorer la fiabilité et l’efficacité de nos processus opérationnels.
         </div>
       </div>
-      <div data-aos="zoom-in-left" className="card md:card-side glass shadow-xl self-end max-w-3xl max-h-3xl">
+      <div data-aos="zoom-in-left" className="card md:card-side glass shadow-xl self-end max-h-3xl">
         <figure className="">
           <img src="img/ia/d.webp" className="md:h-full" />
         </figure>
         <div className="card-body">
           <h2 className="card-title fluid-3xl">Ma vision</h2>
           <p>
-            Un informaticien diot pouvoir mesurer ses efforts. S’il veut pouvoir se concentrer pleinement sur du code à forte valeur ajoutée, il est
-            indispensable que les tâches fastidieuses et répétitives lui prennent peu de temps et d’efforts. C’est dans cet esprit que les IDE progressent et
-            que le DevOps et l’agilité ont vu le jour.
+            Un développeur doit être en mesure d’évaluer son travail. Pour se focaliser efficacement sur des codes hautement stratégiques, il est crucial de
+            minimiser le temps et l’énergie consacrés aux tâches monotones et récurrentes. C’est dans cette optique que les environnements de développement
+            intégrés (IDE) évoluent et que les concepts de DevOps et d’agilité ont été créés.
           </p>
           <p>
-            Docker, les micro-services, NoSQL, le Big Data, toutes ces technologies sont incroyables. Mais ce ne sont que des outils au service d’une meilleure
-            expérience utilisateur. Le choix d’introduire un nouveau framework ne doit être guidé que pour résoudre une problématique, ce qui, in fine,
-            améliorera le quotidien de nos utilisateurs ou même des membres de l’équipe.
+            Docker, les architectures micro-services, les bases de données NoSQL et le Big Data sont des technologies impressionnantes. Cependant, elles ne sont
+            que des instruments destinés à enrichir l’expérience des utilisateurs. L’adoption d’un nouveau framework devrait uniquement être motivée par le
+            besoin de répondre à un défi spécifique, ce qui, en définitive, bénéficiera à la fois aux utilisateurs et à l’équipe dans leur ensemble.
           </p>
           <p>
-            L’informatique est en perpétuel évolution et nous oblige à nous tenir informés des nouvelles avancées. La veille technologies fait partie intégrante
-            de notre métier.
+            L’informatique, caractérisée par son évolution rapide et continue, exige de nous une vigilance constante pour suivre les avancées technologiques. La
+            veille technologique n’est pas seulement une recommandation, c’est une composante essentielle de notre activité professionnelle. Elle nous permet de
+            rester compétitifs et innovants, en anticipant les tendances et en intégrant de nouvelles pratiques qui améliorent notre travail et celui de nos
+            collègues.
           </p>
           <p>
-            Agilité et DevOps créent une révolution dans la gestion des projets informatiques. Lorsque l’on a testé les méthodes agiles et l’amélioration
-            continue, il est impossible de revenir à une gestion classique et des cycles en V.
+            Les approches Agilité et DevOps représentent une véritable révolution dans la manière de gérer les projets informatiques. Elles introduisent des
+            pratiques qui transforment radicalement nos méthodes de travail, favorisant la flexibilité, la collaboration et la réactivité. Une fois que l’on a
+            expérimenté la fluidité des méthodes agiles et l’efficacité de l’amélioration continue, il devient impensable de revenir aux méthodologies
+            traditionnelles rigides et aux cycles de développement en V. Ces nouvelles méthodes nous permettent de mieux répondre aux besoins changeants des
+            clients et d’optimiser la qualité de nos produits de manière constante et mesurable.
           </p>
         </div>
       </div>
